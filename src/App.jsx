5a7799ddf5048ccc31a7098446048ef9b8a726c7
ikanu8910 <ikanu8910@yahoo.com>
2024-06-21 14:53:40 -0600

    Header and Intro Placeholder
    Text, menu logos, and placeholder for intro animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import Description from './components/Description/Description'
+import Navbar from './components/Navbar/Navbar'
 
 const App = () => {
   return (
-    <div>
-      export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <div className="overflow-x-hidden text-neutral-950 antialised selection:bg-purple-400 selection:text-neutral-100">
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      
+      <div className="container mx-auto px-8">
+
+      <Navbar />
+      <Description />
+
+
+    </div>
     </div>
   )
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import "./Navbar.css"
+
+import { SiAboutdotme } from "react-icons/si"
+import { GoProjectRoadmap } from "react-icons/go"
+import { FaGithub } from "react-icons/fa"
+
+const Navbar = () => {
+  return (
+    <header className = "Navbar">
+      <nav className="mb-20 flex items-center justify-between py-6">
+          <>Isha Kanu</>
+          
+          <div className="m-8 flex items-center justify-center gap-4 py-2xl">
+          <SiAboutdotme />
+          <GoProjectRoadmap />
+          <FaGithub />
+          </div>
+      
+      </nav>
+    </header>
+
+  )
+}
+
+export default Navbar
