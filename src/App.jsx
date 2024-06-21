96a4043464c342dd5e07a87017073f095aa81f18
ikanu8910 <ikanu8910@yahoo.com>
2024-06-21 10:39:10 -0600

    Beginning of port
    Initial downloads and configurations

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
+    </div>
+  )
+}
+
+export default App
