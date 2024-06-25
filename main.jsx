1f907ca02a44189aef10388e3ede123e72b34ca8
ikanu8910 <ikanu8910@yahoo.com>
2024-06-24 22:36:58 -0600

    Updating path

--- main.jsx
+++ main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import App from '/Users/isha.kanu/Documents/GitHub/IKPort/src/App.jsx'
+import '/Users/isha.kanu/Documents/GitHub/IKPort/src/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
