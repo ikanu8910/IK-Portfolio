a6b0be8ebe82df4553b9823450872ce4030f572f
ikanu8910 <ikanu8910@yahoo.com>
2024-06-24 21:45:56 -0600

    Updating portfolio to main

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,30 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+
+import Projects from './components/Projects'
+import AboutMe from './components/AboutMe'
+import Description from './components/Description'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
+const main = () => {
+  return (
+    <div className="overflow-x-hidden text-neutral-950 antialised selection:bg-purple-400 selection:text-neutral-100">
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      
+      <div className="container mx-auto px-8">
+
+      <Navbar />
+      <Description />
+      <AboutMe />
+      <Projects />
+      <Footer/>
+
+
+    </div>
+    </div>
+  )
+}
+
+export default App
