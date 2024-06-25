e9094e3ea877f19b237490db1c32bb95f04862d3
ikanu8910 <ikanu8910@yahoo.com>
2024-06-25 14:58:05 -0600

    Update Description.jsx

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -9,7 +9,7 @@ const Description = () => {
             initial={{x: -100, opacity: 0}}
             animate={{x: 0, opacity: 1}}
             transition={{ duration: 1.25, delay: 0}} 
-            className="font-bold text-7xl/relaxed s:text-2xl/relaxed text-balance text-center bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text tracking-tight text-transparent">Designer & Creative Technologist</motion.h1>
+            className="font-bold text-7xl/relaxed sm:text-2xl/relaxed text-balance text-center bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text tracking-tight text-transparent">Designer & Creative Technologist</motion.h1>
         </div>
     </div>
 
